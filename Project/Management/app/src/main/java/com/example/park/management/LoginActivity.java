baed70a5f998e140435bace04a189af70af386b6
ParkBeomMin <club20608@gmail.com>
2017-01-11 15:57:39 +0900

    app

---
+++ Project/Management/app/src/main/java/com/example/park/management/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.park.management;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        Button loginButton = (Button) findViewById(R.id.loginbutton);
+        Button registerButton = (Button) findViewById(R.id.registerbutton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
---
+++ Project/Management/app/src/main/java/com/example/park/management/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.example.park.management;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Park on 2017-01-11.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    final static private String URL = "http://jisung0920.cafe24.com/Register.php";
+    private Map<String, String> parameters;
+
+    public RegisterRequest(String  userID, String userPassword, String userName, int userStudentNumber, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+        parameters.put("userName", userName);
+        parameters.put("userStudentNumber", userStudentNumber + "");
+    }
+
+    public Map<String, String > getParams() {
+        return parameters;
+    }
+}
