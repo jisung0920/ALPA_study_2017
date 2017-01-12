94eee19f89ac61498e3919cc6a59b260f224834e
ParkBeomMin <club20608@gmail.com>
2017-01-12 18:24:46 +0900

    studentnumberspinner

--- Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/RegisterActivity.java
+++ Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/RegisterActivity.java
@@ -1,12 +1,14 @@
 package com.example.park.management;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -16,13 +18,19 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.example.park.management.R.id.idcheckbutton;
-import static com.example.park.management.R.id.idcheckbutton;
 import static com.example.park.management.R.id.registerbutton;
+import static com.example.park.management.R.id.validatebutton;
 
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+    private String userID;
+    private String userPassword;
+    private  String userName;
+    private  String StudentNumber;
+    private AlertDialog dialog;
+    private boolean validate = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,16 +40,63 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText passwordText = (EditText) findViewById(R.id.passwordText);
         final EditText password2Text = (EditText) findViewById(R.id.password2Text);
         final EditText nameText = (EditText) findViewById(R.id.nameText);
-        final EditText studentnumberText = (EditText) findViewById(R.id.studentnumberText);
 
-        Button registerButton = (Button) findViewById(registerbutton);
-        Button idcheckButton = (Button) findViewById(idcheckbutton);
+        final Button registerButton = (Button) findViewById(registerbutton);
+        final Button validateButton = (Button) findViewById(validatebutton);
 
-        idcheckButton.setOnClickListener(new View.OnClickListener() {
+        spinner = (Spinner) findViewById(R.id.studentnumberspinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.studentnumber, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userID = idText.getText().toString();
 
+                String userID = idText.getText().toString();
+                if(validate) {
+                    return;
+                }
+                if(userID.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디를 입력해주세요")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할수 있는 아이디입니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                idText.setEnabled(false);
+                                validate = true;
+                                idText.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                                validateButton.setBackgroundColor(getResources().getColor(R.color.colorGray));
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할수 없는 아이디입니다.")
+                                        .setNegativeButton("확인", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userID, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
             }
         });
 
@@ -52,7 +107,16 @@ public class RegisterActivity extends AppCompatActivity {
                 final String userPassword = passwordText.getText().toString();
                 final String userPassword2 = password2Text.getText().toString();
                 final String userName = nameText.getText().toString();
-                final int userStudentNumber = Integer.parseInt(studentnumberText.getText().toString());
+                final String userStudentNumber = spinner.getSelectedItem().toString();
+
+                if(!validate) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디 중복체크를 해주세요.")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
 
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -60,7 +124,7 @@ public class RegisterActivity extends AppCompatActivity {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success ;
-                            if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == 00) {
+                            if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == "") {
                                 success = false;
                             }
                             else {
@@ -83,7 +147,7 @@ public class RegisterActivity extends AppCompatActivity {
                             }
                             else{
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == 00) {
+                                if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == "") {
                                     builder.setMessage("회원정보를 모두 입력해주세요.")
                                             .setNegativeButton("다시시도", null)
                                             .create()
--- Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/ValidateRequest.java
+++ Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/ValidateRequest.java
@@ -11,18 +11,15 @@ import java.util.Map;
  * Created by Park on 2017-01-11.
  */
 
-public class RegisterRequest extends StringRequest {
+public class ValidateRequest extends StringRequest {
 
-    final static private String URL = "http://jisung0920.cafe24.com/Register.php";
+    final static private String URL = "http://jisung0920.cafe24.com/Validate.php";
     private Map<String, String> parameters;
 
-    public RegisterRequest(String  userID, String userPassword, String userName, String userStudentNumber, Response.Listener<String> listener) {
+    public ValidateRequest(String  userID, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
         parameters = new HashMap<>();
         parameters.put("userID", userID);
-        parameters.put("userPassword", userPassword);
-        parameters.put("userName", userName);
-        parameters.put("userStudentNumber", userStudentNumber + "");
     }
 
     public Map<String, String> getParams() {
