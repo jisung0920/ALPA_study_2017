a7930f6ef78b0c57ccca6e383f9ba17cde8a0658
ParkBeomMin <club20608@gmail.com>
2017-01-12 14:27:56 +0900

    register

--- Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/MainActivity.java
+++ Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/MainActivity.java
@@ -1,32 +1,102 @@
 package com.example.park.management;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private  long lastTimeBackPressed;
+    private ListView noticeListView;
+    private NoticListAdapter adapter;
+    private List<Notice> noticeList;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView idText = (TextView) findViewById(R.id.idText);
-        TextView passwordText = (TextView) findViewById(R.id.passwordText);
-        TextView welcomMessage = (TextView) findViewById(R.id.welcomMessage);
+        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        noticeList.add(new Notice("공지사항","박범민","2017-01-01"));
+        adapter = new NoticListAdapter(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
 
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPassword = intent.getStringExtra("userPassword");
-        String message = "환영" + userID + "님";
 
-        idText.setText(userID);
-        passwordText.setText(userPassword);
-        welcomMessage.setText(message);
+        final Button courseButton = (Button) findViewById(R.id.courseButton);
+        final Button statisticsButton = (Button) findViewById(R.id.statisticsButton);
+        final Button scheduleButton = (Button) findViewById(R.id.scheduleButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+
+
+        courseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new CourseFragment());
+                fragmentTransaction.commit();
+
+
+            }
+        });
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new CourseFragment());
+                fragmentTransaction.commit();
+
+
+            }
+        });
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                notice.setVisibility(View.GONE);
+                courseButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                scheduleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new CourseFragment());
+                fragmentTransaction.commit();
+
+
+            }
+        });
     }
 
     public void onBackPressed() {
--- Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/RegisterActivity.java
+++ Member/13_BeomMin/app/Management/app/src/main/java/com/example/park/management/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -15,6 +16,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static com.example.park.management.R.id.idcheckbutton;
+import static com.example.park.management.R.id.idcheckbutton;
 import static com.example.park.management.R.id.registerbutton;
 
 
@@ -27,39 +30,70 @@ public class RegisterActivity extends AppCompatActivity {
 
         final EditText idText = (EditText) findViewById(R.id.idText);
         final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText password2Text = (EditText) findViewById(R.id.password2Text);
         final EditText nameText = (EditText) findViewById(R.id.nameText);
-        final EditText ageText = (EditText) findViewById(R.id.ageText);
+        final EditText studentnumberText = (EditText) findViewById(R.id.studentnumberText);
 
         Button registerButton = (Button) findViewById(registerbutton);
+        Button idcheckButton = (Button) findViewById(idcheckbutton);
+
+        idcheckButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = idText.getText().toString();
+
+            }
+        });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                String userID = idText.getText().toString();
-                String userPassword = passwordText.getText().toString();
-                String userName = nameText.getText().toString();
-                int userStudentNumber = Integer.parseInt(ageText.getText().toString());
+                final String userID = idText.getText().toString();
+                final String userPassword = passwordText.getText().toString();
+                final String userPassword2 = password2Text.getText().toString();
+                final String userName = nameText.getText().toString();
+                final int userStudentNumber = Integer.parseInt(studentnumberText.getText().toString());
+
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
+                            boolean success ;
+                            if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == 00) {
+                                success = false;
+                            }
+                            else {
+                                if(userPassword == userPassword2) {
+                                    success = true;
+                                }
+                                else {
+                                    success = false;
+                                }
+                            }
                             if(success) {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원등록에 성공하셨습니다.")
-                                        .setPositiveButton("확인", null)
-                                        .create()
-                                        .show();
+                                    builder.setMessage("회원가입되셨습니다.")
+                                            .setPositiveButton("확인", null)
+                                            .create()
+                                            .show();
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 RegisterActivity.this.startActivity(intent);
                             }
                             else{
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원등록에 실패.")
-                                        .setNegativeButton("다시시도", null)
-                                        .create()
-                                        .show();
+                                if(userID == "" || userPassword == "" || userPassword2 == "" || userName == "" || userStudentNumber == 00) {
+                                    builder.setMessage("회원정보를 모두 입력해주세요.")
+                                            .setNegativeButton("다시시도", null)
+                                            .create()
+                                            .show();
+                                }
+                                else if(userPassword != userPassword2) {
+                                    builder.setMessage("2차 비밀번호가 다릅니다.")
+                                            .setNegativeButton("다시시도", null)
+                                            .create()
+                                            .show();
+                                }
                             }
                         }
                         catch (JSONException e) {
