58775b3e2ef180495c54a6ba1ab7c75927880dd5
JaeMinL <sjsk03026@naver.com>
2017-01-12 17:00:04 +0900

    app

---
+++ Member/15_JaeMin/app/src/main/java/com/example/user/hio/AnnoucementLayout.java
@@ -0,0 +1,22 @@
+package com.example.user.hio;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2017-01-11.
+ */
+
+public class AnnoucementLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.annoucement_layout, container, false);
+        return v;
+    }
+}
