54c218dd1c224f10e03781c26926a5ca5bd7eae8
jisung0920 <jisung0920@gmail.com>
2017-01-29 19:48:41 +0900

    topcoderI

--- Member/13_jisung/week4_Topcoder/src/Cryptography/Cryptography.java
+++ Member/13_jisung/week4_Topcoder/src/Cryptography/Cryptography.java
@@ -21,10 +21,9 @@ public class Cryptography {
 	}*/
 	
 	public long encrypt(int[] numbers){
-		Arrays.sort(numbers);
-		int N = numbers.length;
-		int MAX = numbers[0]+1;
-		for(int i=1;i<N;i++)
+		Arrays.sort(numbers); // array 정렬 
+		int MAX = numbers[0]+1; //배열의 최소값에 1을 더한다.
+		for(int i=1;i<numbers.length;i++) //배열의 요소 곱한다.
 			MAX*=numbers[i];
 		return MAX;	
 	}
