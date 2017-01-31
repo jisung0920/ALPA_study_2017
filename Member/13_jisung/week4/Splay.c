5f2a4af3ec7ca154890aaeb68c8c587e79c127a1
jisung <jisung0920@gmail.com>
2017-01-31 16:18:23 +0900

    LLRR

--- Member/13_jisung/week4/Splay.c
+++ Member/13_jisung/week4/Splay.c
@@ -10,98 +10,113 @@ Tree* MakeTree(void){
   return tree;
 }
 
-void Zig_Par_Setting(Node* x,Node* y){
-  Node* z = y->par;
-  if(z->left==y)
-    z->left = x;
+Node* Zig_L(Node* bt){
+  printf("Rotate Left %d\n",bt->right->data);
+  Node* pnode = bt;
+  Node* cnode = pnode->right;
+  if(ChildNodeState(cnode) == Zero ||ChildNodeState(cnode) ==One_Right)
+    pnode->right =NULL;
   else
-    z->right = y;
+    pnode->right = cnode->left;
+  cnode->left = pnode;
+  pnode->par = cnode;
+  return cnode;
 }
 
-Node* Zig_L(Node* y){
-  Node* x = y->right;
-  if(y->par != NULL)
-    Zig_Par_Setting(x,y);
-  y->right = x->left;
-  x->left = y;
-  y->par = x;
-  return x;
-}
-
-Node* Zig_R(Node* y){
-  Node* x = y->left;
-  if(y->par != NULL)
-    Zig_Par_Setting(x,y);
-  y->left = x->right;
-  x->right = y;
-  y->par = x;
-  return x;
+Node* Zig_R(Node* bt){
+    printf("Rotate Right %d\n",bt->left->data);
+  Node* pnode = bt;
+  Node* cnode = pnode->left;
+  if(ChildNodeState(cnode) == Zero ||ChildNodeState(cnode) ==One_Left)
+    pnode->left =NULL;
+  else
+    pnode->left = cnode->right;
+  cnode->right = pnode;
+  pnode->par = cnode;
+  return cnode;
 }
 
 Node* Zig_Zig_L( Node* node){
-  node = Zig_L(node);
+  printf("LL\n");
+  node->right = Zig_L(node->right);
   node = Zig_L(node);
   return node;
 }
 Node* Zig_Zig_R( Node* node){
-  node = Zig_R(node);
+  printf("RR\n");
+  node->left = Zig_R(node->left);
   node = Zig_R(node);
   return node;
 }
 
 Node* Zig_Zag_RL(Node* node){
-  node = Zig_R(node);
+  printf("RL\n");
+  node->right->left = Zig_R(node->right->left);
   node = Zig_L(node);
   return node;
 }
 
 Node* Zig_Zag_LR(Node* node){
-  node = Zig_L(node);
+  printf("LR\n");
+  node->left = Zig_L(node->left);
+  printf("LR - 2nd\n");
   node = Zig_R(node);
   return node;
 }
 
 void ZigZag(Tree* tree,Node* node){
+  printf("Zig Zag Function :%d\n",node->data);
   Node* Root = tree->root;
-  Node* Pnode = node->par;
   while(1){
-      if(Root == node)
-        break;
-      else if(Root->left == node)
-        Pnode = Zig_R(Pnode);
-      else if(Root->right == node)
-        Pnode = Zig_L(Pnode->par);
-      else{
-        /*Node* Gnode = node->par;
-        if(Gnode->left == node)
-          Zig_R(node->par);
-        else if(Gnode->right == node)
-          Zig_L(node->par);}*/
-        //Node* Gnode = node->par->par;
+    if(tree->root == node)
+      break;
+    else if(Root->left == node){
+      tree->root = Zig_R(node->par);
+      break;
+    }
+    else if(Root->right == node){
+      tree->root= Zig_L(node->par);
+      break;
+    }
+    else{
+      printf("else Case\n");
+      Node* Gnode = node->par->par;
 
-        Node* Gnode = Pnode->par;
-        if(Gnode->left == Pnode){
-          if(Pnode->left == node)
-            Pnode = Zig_Zig_R(Pnode);
+      printf("Gnode data : %d\n",Gnode->data);
+      if(Gnode == tree->root){
+        if(Gnode->left == node->par){
+          if(node->par->left == node)
+            Gnode = Zig_Zig_R(Gnode);
           else
-            Pnode = Zig_Zag_RL(Pnode);
-          }
+            Gnode = Zig_Zag_LR(Gnode);
+        }
         else{
-          if(Pnode->right == node)
-            Zig_Zig_L(Pnode);
+          if(node->par->left == node)
+            Gnode = Zig_Zag_RL(Gnode);
           else
-            Zig_Zag_LR(Pnode);
+            Gnode = Zig_Zig_L(Gnode);
+        }
+        tree->root = Gnode;
+        break;
+      }
+      else{
+        if(Gnode->left == node->par){
+          if(node->par->left == node)
+            Gnode = Zig_Zig_R(Gnode);
+          else
+            Gnode = Zig_Zag_LR(Gnode);
+        }
+        else{
+          if(node->par->left == node)
+            Gnode = Zig_Zag_RL(Gnode);
+          else
+            Gnode = Zig_Zig_L(Gnode);
         }
 
-        /*else if(Gnode->right->right == node)
-        else if(Gnode->left->right == node)
-          Zig_Zag_RL(node->par);
-        else
-          Zig_Zag_LR(node->par);  */
-
       }
+
     }
-    tree->root = node;
+  }
 }
 
 Node* S_SearchNode(Tree* tree,Data data){
--- Member/13_jisung/week4/main_splay.c
+++ Member/13_jisung/week4/main_splay.c
@@ -2,12 +2,35 @@
 
 int main(void){
   Tree* tree=MakeTree();
-  tree->root = MakeNode(4);
+  tree->root = MakeNode(3);
+
+  InsertTree(tree,MakeNode(4));
+  PreorderTraverse(tree->root);
+  putchar('\n');
+  //InsertTree(tree,MakeNode(2));
+    //InsertTree(tree,MakeNode(1));
+  //tree->root = Zig_R(tree->root);
 
   InsertTree(tree,MakeNode(5));
-//  InsertTree(tree,MakeNode(6));
-/*  InsertTree(tree,MakeNode(7));
-  InsertTree(tree,MakeNode(1));
+    PreorderTraverse(tree->root);
+      putchar('\n');
+    InsertTree(tree,MakeNode(8));
+        PreorderTraverse(tree->root);
+          putchar('\n');
+          putchar('\n');
+  //  InsertTree(tree,MakeNode(2));
+        PreorderTraverse(tree->root);
+          putchar('\n');
+          putchar('\n');
+    InsertTree(tree,MakeNode(9));
+    PreorderTraverse(tree->root);
+
+          putchar('\n');
+          putchar('\n');
+    InsertTree(tree,MakeNode(6));
+        InsertTree(tree,MakeNode(9));
+//    tree->root = Zig_L(tree->root);
+/*  InsertTree(tree,MakeNode(1));
   //InsertTree(tree,MakeNode(8));//한방향만 가능
   InsertTree(tree,MakeNode(1));*/
   PreorderTraverse(tree->root);
